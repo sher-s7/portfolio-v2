f4a1bc52fc5236fba1b607f3abc6573cd290ad4d
sher-s7 <sher.sheikh77@gmail.com>
2022-06-12 19:20:50 -0400

    Set up gradient text

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+const theme = {
+  fonts: {
+    body: "Inter, system-ui, sans-serif",
+    heading: 'Inter, "Avenir Next", sans-serif',
+    monospace: '"IBM Plex Mono", Menlo, monospace',
+  },
+  fontWeights: {
+    extralight: 200,
+    light: 300,
+    semibold: 600,
+  },
+  styles: {
+    root: {
+      fontFamily: "body",
+      fontWeight: "light",
+    },
+  },
+  colors: {
+    text: "#000",
+    background: "#000",
+  },
+};
+
+export default theme;
