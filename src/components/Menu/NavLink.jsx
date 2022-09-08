7c08e20b8cab460552103e48439b0328b4156a71
sher-s7 <sher.sheikh77@gmail.com>
2022-09-08 01:23:25 -0400

    Complete styling for nav links

---
+++ src/components/Menu/NavLink.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { Box, Link } from "theme-ui";
+
+const TRANSITION_LENGTH = 0.75;
+
+const NavLink = ({ navItem }) => {
+  const [clicked, setClicked] = useState(false);
+  const [clickCoords, setClickCoords] = useState({ x: 0, y: 0 });
+  const [clickTimeout, setClickTimeout] = useState(null);
+  return (
+    <Box
+      className={`${clicked ? "clicked" : ""}`}
+      sx={{
+        overflow: "hidden",
+        borderRadius: "6px",
+        position: "relative",
+        transition: "0.2s ease-out, transform 0.1s linear",
+        borderTop: "1px solid transparent",
+        a: {
+          p: "10px 15px",
+          display: "block",
+          cursor: "pointer",
+          "-webkit-tap-highlight-color": "transparent"
+        },
+        "::after": {
+          content: "''",
+          pointerEvents: "none",
+          transition: "0.1s ease-out",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          background:
+            "linear-gradient(45deg, transparent 0%, rgba(255, 255, 255, 0.2) 100%)",
+          opacity: 0
+        },
+        "::before": {
+          content: '"⬤"',
+          pointerEvents: "none",
+          position: "absolute",
+          top: clickCoords.y + "px",
+          left: clickCoords.x + "px",
+          transform: "translate(-50%, -50%) scale(0)",
+          fontFamily: "Arial",
+          fontSize: "20rem",
+          color: "rgba(255,255,255,0.1)",
+          transition: "0s"
+        },
+        ":hover": {
+          borderColor: [null, "rgba(255, 255, 255, 0.1)"],
+          boxShadow: [null, "-3px 5px 15px rgba(0, 0, 0, 0.2)"],
+          "::after": {
+            opacity: [null, 1]
+          }
+        },
+        ":active": {
+          transition: "box-shadow 0.1s linear",
+          transform: "scale(0.97)",
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)"
+        },
+        "&.clicked::before": {
+          transform: "translate(-50%, -50%) scale(1)",
+          opacity: 0,
+          transition: `transform ${TRANSITION_LENGTH}s ease, opacity ${TRANSITION_LENGTH}s ease`
+        }
+      }}
+    >
+      <Link
+        variant="nav"
+        href={`#${navItem}`}
+        onClick={(event) => {
+          let currentTargetRect = event.currentTarget.getBoundingClientRect();
+          const event_offsetX = event.pageX - currentTargetRect.left,
+            event_offsetY = event.pageY - currentTargetRect.top;
+          setClickCoords({
+            x: parseInt(event_offsetX),
+            y: parseInt(event_offsetY)
+          });
+
+          setClicked(false);
+          clearTimeout(clickTimeout);
+          setTimeout(() => {
+            setClicked(true);
+          }, 10);
+
+          setClickTimeout(
+            setTimeout(() => {
+              setClicked(false);
+            }, TRANSITION_LENGTH * 1000)
+          );
+        }}
+      >
+        {navItem}
+      </Link>
+    </Box>
+  );
+};
+
+export default NavLink;
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,8 +1,12 @@
+import { keyframes } from "@emotion/react";
+import { useState } from "react";
 import { Box, Flex, Heading, Image, Link, Text } from "theme-ui";
 import { NAV_ITEMS } from "../../utils/constants";
 import github from "./icons/github.png";
+import NavLink from "./NavLink";
 
 const Main = () => {
+  const [clicked, setClicked] = useState(null);
   return (
     <Flex
       as="section"
@@ -27,54 +31,11 @@ const Main = () => {
           maxWidth: ["375px", "none"],
           gap: ["0 22px", null, "0 48px"],
           flexWrap: "wrap",
-          justifyContent: "center",
-          div: {
-            overflow: "hidden",
-            borderRadius: "6px",
-            position: "relative",
-            transition: "0.2s ease-out, transform 0.1s linear",
-            borderTop: "1px solid transparent",
-            a: {
-              p: "10px 15px",
-              display: "block",
-              cursor: "pointer"
-            },
-            "::after": {
-              content: "''",
-              pointerEvents: "none",
-              transition: "0.2s ease-out",
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              background:
-                "linear-gradient(45deg, transparent 0%, rgba(255, 255, 255, 0.2) 100%)",
-              opacity: 0
-            },
-            ":hover": {
-              borderColor: "rgba(255, 255, 255, 0.1)",
-              boxShadow: "-3px 5px 15px rgba(0, 0, 0, 0.2)",
-              "::after": {
-                opacity: 1
-              }
-            },
-            ":active": {
-              transition: "box-shadow 0.1s linear",
-              transform: "scale(0.97)",
-              boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)"
-            }
-          }
+          justifyContent: "center"
         }}
       >
-        {Object.keys(NAV_ITEMS).map((key) => {
-          return (
-            <Box>
-              <Link variant="nav" href={`#${NAV_ITEMS[key]}`}>
-                {NAV_ITEMS[key]}
-              </Link>
-            </Box>
-          );
+        {Object.keys(NAV_ITEMS).map((key, i) => {
+          return <NavLink navItem={NAV_ITEMS[key]} key={key + i} />;
         })}
       </Flex>
     </Flex>
