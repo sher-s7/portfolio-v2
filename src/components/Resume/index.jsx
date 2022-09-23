5a1581e456a3b6ae9c4bf51e5c41eecb5b6633c8
sher-s7 <sher.sheikh77@gmail.com>
2022-09-23 19:10:43 -0400

    Restyle toast and add resume links

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -14,8 +14,17 @@ const Resume = () => {
             gap: ["10px", "18px"]
           }}
         >
-          <CircleFillBtn>Download PDF</CircleFillBtn>
-          <CircleFillBtn preview>View online</CircleFillBtn>
+          <CircleFillBtn href="./resume.pdf" download>
+            Download PDF
+          </CircleFillBtn>
+          <CircleFillBtn
+            href="https://drive.google.com/file/d/1oUjeYEGc-4FOEKF7Vn2Tkw8PysAjPlxf/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+            preview
+          >
+            View online
+          </CircleFillBtn>
         </Flex>
       </Flex>
     </Flex>
--- src/components/common/Toast/index.jsx
+++ src/components/common/Toast/index.jsx
@@ -36,13 +36,14 @@ const Toast = ({ length, index, close, children }) => {
       sx={{
         fontSize: [1, 2],
         padding: "10px 10px",
-        borderRadius: "3px",
+        borderRadius: "6px",
         animation: `${fadeInOut} ${length / 1000}s ease`,
         animationFillMode: "both",
         fontWeight: "bold",
-        textShadow: "-5px 5px 10px rgba(0,0,0,0.5)",
+        background: "linear-gradient(45deg, #181818 0%, #3d3d3d 100%)",
+        boxShadow: "-3px 5px 15px rgba(0, 0, 0, 0.2)",
         position: "absolute",
-        transform: `translateY(calc(-100% * ${index * 1.1}))`,
+        transform: `translateY(calc(-100% * ${index * 1.2}))`,
         transition: "transform 0.25s ease",
         pointerEvents: "none"
       }}
